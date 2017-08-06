5ce35e3d61aefb153511dfe0a49e4819087c1899
Taylor Caton <taylorcaton@gmail.com>
2017-08-06 19:47:11 -0400

    removed modules

--- app.js
+++ app.js
@@ -1,12 +1,4 @@
 var BasicCard = require('./BasicCard.js');
 var ClozeCard = require('./ClozeCard.js');
-
-var firstPresident = new BasicCard("Who was our first president?", "George Washington");
-console.log(firstPresident.front);
-console.log(firstPresident.back);
-
-var firstPresidentCloze = new ClozeCard("George Washington was our first president", "George Washington");
-console.log(firstPresidentCloze.cloze);
-firstPresident.partial = firstPresidentCloze.getPartial();
-console.log(firstPresidentCloze.partial);
-console.log(firstPresidentCloze.fullText);
+var fs = require('fs');
+var inquirer = require('inquirer');
